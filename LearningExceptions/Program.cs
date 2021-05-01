f00c0b9615bc21c4efb19ea8529aa1d3a7b11eff
amitksms97 <amitksms97@gmail.com>
2021-05-01 20:31:56 +0530

    Add project files.

---
+++ LearningExceptions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LearningExceptions
+{
+    class Person
+    {
+        private String name;
+
+        public String Name
+        {
+            get { return name; }
+            set 
+            { if(value == null)
+                throw new NullReferenceException("name must have a value");
+                name = value;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var person = new Person();
+                person.Name = null;
+                //throw new Exception("custom message");
+                //string s = null;
+                //var l = s.Length;
+            }
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("disconnecting database");
+            }
+            
+        }
+    }
+}
